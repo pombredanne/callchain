d05015b3ee8a818c5e67fa7eb7a56ab31bab0218
lcr <kwarterthieves@yahoo.com>
2012-03-09 17:34:12 -0700

    - fix ref
    --HG--
    branch : pu

--- callchain/root/linked.py
+++ callchain/root/linked.py
@@ -2,13 +2,14 @@
 '''active queued linked call chains'''
 from callchain.root.mixins import RootMixin
 from callchain.assembly.linked import LinkedChain, Eventlink
+from callchain.mixin.active import ActiveCallMixin, ActiveECallMixin
 
 
-class chainlink(RootMixin, LinkedChain):
+class chainlink(RootMixin, ActiveCallMixin, LinkedChain):
 
     '''root linked chain'''
 
 
-class eventlink(RootMixin, Eventlink):
+class eventlink(RootMixin, ActiveECallMixin, Eventlink):
 
     '''root linked event chain'''
