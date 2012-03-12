2cec85e58cf7a75eb4c8c74d9e29232aa03997c9
lcr <kwarterthieves@yahoo.com>
2012-03-11 23:52:18 -0600

    - fix keys
    --HG--
    branch : pu

--- callchain/keys/chain.py
+++ callchain/keys/chain.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 '''chain keys'''
 
-from callchain.keys.call import KChainCall, KEventCall
-from callchain.keys.manager import KChainManager, KEventManager
-from callchain.keys.root import KChainRoot, KEventRoot
-from callchain.keys.fluent import KChain, KEvent
 from callchain.keys.queued import KQueued
+from callchain.keys.fluent import KChain, KEvent
+from callchain.keys.call import KChainCall, KEventCall
+from callchain.keys.root import KChainRoot, KEventRoot
+from callchain.keys.manager import KChainManager, KEventManager
 
 
 class KCallChain(KChainCall, KChainManager, KChainRoot, KChain):
