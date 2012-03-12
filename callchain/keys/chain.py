1d20df260423b929c0fbecc80a4968efb62f5c55
lcr <kwarterthieves@yahoo.com>
2012-03-11 20:38:45 -0600

    - back
    --HG--
    branch : pu

---
+++ callchain/keys/chain.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''chain keys'''
+
+from callchain.keys.call import KChainCall, KEventCall
+from callchain.keys.manager import KChainManager, KEventManager
+from callchain.keys.root import KChainRoot, KEventRoot
+from callchain.keys.fluent import KChain, KEvent
+from callchain.keys.queued import KQueued
+
+
+class KCallChain(KChainCall, KChainManager, KChainRoot, KChain):
+
+    '''call chain key'''
+
+
+class KCallChainQ(KCallChain, KQueued):
+
+    '''queued call chain key'''
+
+
+class KEventChain(KEventCall, KEventManager, KEventRoot, KEvent):
+
+    '''event chain key'''
+
+
+class KEventChainQ(KEventChain, KQueued):
+
+    '''queued event chain key'''
