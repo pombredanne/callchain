cb9ca9519db6b89abb6337d02111c74ed49c2cdd
*** failed to import extension hggit: No module named hggit <none@none>
2012-03-19 00:17:42 -0600

    - fix import
    --HG--
    branch : pu

--- callchain/active_man/chainlet.py
+++ callchain/active_man/chainlet.py
@@ -9,7 +9,7 @@ from twoq.mixins.ordering import RandomMixin, OrderMixin
 from twoq.mixins.reducing import MathMixin, TruthMixin, ReduceMixin
 from twoq.mixins.mapping import DelayMixin, CopyMixin, RepeatMixin, MapMixin
 
-from callchain.chain import Chainlet
+from callchain.chain import ChainletQ
 from callchain.services.order import KRandom, KOrder
 from callchain.services.reduce import KMath, KReduce, KTruth
 from callchain.services.map import KDelay, KCopy, KRepeat, KMap
@@ -23,78 +23,78 @@ __all__ = (
 
 
 @appifies(KDelay)
-class delaychain(Chainlet, ManQMixin, DelayMixin):
+class delaychain(ChainletQ, ManQMixin, DelayMixin):
 
     '''manually balanced delayed mapping chainlet'''
 
 
 @appifies(KCopy)
-class copychain(Chainlet, ManQMixin, CopyMixin):
+class copychain(ChainletQ, ManQMixin, CopyMixin):
 
     '''manually balanced copy chainlet'''
 
 
 @appifies(KRepeat)
-class repeatchain(Chainlet, ManQMixin, RepeatMixin):
+class repeatchain(ChainletQ, ManQMixin, RepeatMixin):
 
     '''manually balanced repeat chainlet'''
 
 
 @appifies(KMap)
-class mapchain(Chainlet, ManQMixin, MapMixin):
+class mapchain(ChainletQ, ManQMixin, MapMixin):
 
     '''manually balanced mapping chainlet'''
 
 
 @appifies(KCollect)
-class collectchain(Chainlet, ManQMixin, CollectMixin):
+class collectchain(ChainletQ, ManQMixin, CollectMixin):
 
     '''manually balanced collecting chainlet'''
 
 
 @appifies(KSet)
-class setchain(Chainlet, ManQMixin, SetMixin):
+class setchain(ChainletQ, ManQMixin, SetMixin):
 
     '''manually balanced seting chainlet'''
 
 
 @appifies(KSlice)
-class slicechain(Chainlet, ManQMixin, SliceMixin):
+class slicechain(ChainletQ, ManQMixin, SliceMixin):
 
     '''manually balanced slicing chainlet'''
 
 
 @appifies(KFilter)
-class filterchain(Chainlet, ManQMixin, FilterMixin):
+class filterchain(ChainletQ, ManQMixin, FilterMixin):
 
     '''manually balanced filtering chainlet'''
 
 
 @appifies(KRandom)
-class randomchain(Chainlet, ManQMixin, RandomMixin):
+class randomchain(ChainletQ, ManQMixin, RandomMixin):
 
     '''manually balanced randomizing chainlet'''
 
 
 @appifies(KOrder)
-class orderchain(Chainlet, ManQMixin, OrderMixin):
+class orderchain(ChainletQ, ManQMixin, OrderMixin):
 
     '''manually balanced ordering chainlet'''
 
 
 @appifies(KMath)
-class mathchain(Chainlet, ManQMixin, MathMixin):
+class mathchain(ChainletQ, ManQMixin, MathMixin):
 
     '''manually balanced mathing chainlet'''
 
 
 @appifies(KReduce)
-class reducechain(Chainlet, ManQMixin, ReduceMixin):
+class reducechain(ChainletQ, ManQMixin, ReduceMixin):
 
     '''manually balanced reducing chainlet'''
 
 
 @appifies(KTruth)
-class truthchain(Chainlet, ManQMixin, TruthMixin):
+class truthchain(ChainletQ, ManQMixin, TruthMixin):
 
     '''manually balanced truthing chainlet'''
