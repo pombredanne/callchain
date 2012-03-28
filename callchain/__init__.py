a009bc61d215c24e6ee0211553c9c2cd3c05aa01
lcr <kwarterthieves@yahoo.com>
2012-03-28 14:34:58 -0600

    - merge
    --HG--
    branch : next

--- callchain/__init__.py
+++ callchain/__init__.py
@@ -6,5 +6,21 @@ from callchain.event import einside
 from callchain.patterns import Pathways, Branchways, Nameways
 from callchain.settings import DefaultSettings, RequiredSettings
 from callchain.mixins.resets import ResetLocalMixin, ResetTypeMixin
+from callchain.active_auto.chain import (
+    chainq as aachainq, callchain as aacallchain)
+from callchain.active_auto.event import (
+    eventq as aaeventq, eventchain as aaeventchain)
+from callchain.active_man.chain import (
+    chainq as amchainq, callchain as amcallchain)
+from callchain.active_man.event import (
+    eventq as ameventq, eventchain as ameventchain)
+from callchain.lazy_auto.chain import (
+    chainq as lachainq, callchain as lacallchain)
+from callchain.lazy_auto.event import (
+    eventq as laeventq, eventchain as laeventchain)
+from callchain.lazy_man.chain import (
+    chainq as lmchainq, callchain as lamcallchain)
+from callchain.lazy_man.event import (
+    eventq as lmeventq, eventchain as lmeventchain)
 
-__version__ = (0, 1, 3)
+__version__ = (0, 2, 2)
