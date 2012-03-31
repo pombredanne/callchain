5beccfe73147a5c1cb039654b64cb03ae72cd510
lcr <kwarterthieves@yahoo.com>
2012-03-31 00:09:29 -0600

    - pep 8
    --HG--
    branch : pu

--- callchain/support.py
+++ callchain/support.py
@@ -2,11 +2,62 @@
 '''callchain support'''
 
 from itertools import count
-from functools import partial, total_ordering
+from functools import partial
 try:
     from Queue import PriorityQueue, Queue, Empty
 except ImportError:
     from queue import PriorityQueue, Queue, Empty  # @UnusedImport
+try:
+    from functools import total_ordering
+except ImportError:
+    def total_ordering(cls):
+        '''
+        class decorator that fills in missing ordering methods
+
+        not available for python versions < 2.7
+        '''
+        convert = {
+            '__lt__': [
+                ('__gt__', lambda self, other: not (
+                    self < other or self == other)),
+                ('__le__', lambda self, other: self < other or self == other),
+                ('__ge__', lambda self, other: not self < other),
+            ],
+            '__le__': [
+                ('__ge__',
+                lambda self, other: not self <= other or self == other),
+                ('__lt__',
+                lambda self, other: self <= other and not self == other),
+                ('__gt__', lambda self, other: not self <= other),
+            ],
+            '__gt__': [
+                ('__lt__', lambda self, other: not (
+                    self > other or self == other)),
+                ('__ge__', lambda self, other: self > other or self == other),
+                ('__le__', lambda self, other: not self > other),
+            ],
+            '__ge__': [
+                ('__le__', lambda self, other: (
+                    not self >= other) or self == other
+                ),
+                ('__gt__',
+                lambda self, other: self >= other and not self == other),
+                ('__lt__', lambda self, other: not self >= other)
+            ]
+        }
+        roots = set(dir(cls)) & set(convert)
+        if not roots:
+            raise ValueError(
+                'must define at least one ordering operation: < > <= >='
+            )
+        # prefer __lt__ to __le__ to __gt__ to __ge__
+        root = max(roots)
+        for opname, opfunc in convert[root]:
+            if opname not in roots:
+                opfunc.__name__ = opname
+                opfunc.__doc__ = getattr(int, opname).__doc__
+                setattr(cls, opname, opfunc)
+        return cls
 
 from stuf.utils import lazy_set, setter
 
